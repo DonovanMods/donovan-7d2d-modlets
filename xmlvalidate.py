492e7aa1ad7ba2b2817480a165811eceda31eb8c
Donovan C. Young <dyoung522@gmail.com>
2019-10-08 13:08:29 -0400

    more refactoring

--- xmlvalidate.py
+++ xmlvalidate.py
@@ -1,18 +1,24 @@
 import getopt
-import os
 import sys
 from copy import copy
 from glob import glob
+from os import scandir
 from pathlib import Path
+
 from colorama import Fore, Style
 from lxml import etree
 
 
+def colortext(color, message):
+    return f'{color}{message}{Style.RESET_ALL}'
+
 # Read all config XML files into one massive XML (this is what 7 days does internally)
+
+
 def readConfigs(path):
     XML = etree.Element('root')
 
-    with os.scandir(path) as configs:
+    with scandir(path) as configs:
         for config in configs:
             filename = Path(config)
 
@@ -29,23 +35,27 @@ def readModlets(original_configs):
         configs = copy(original_configs)
         passfail = True
         base_filename = Path(modlet_dir).parent
+        parser = etree.XMLParser(ns_clean=True, remove_comments=True)
 
         if options['verbose']:
-            print(f'{Fore.YELLOW}Checking {base_filename}:{Style.RESET_ALL}', end='')
+            print(f'{colortext(Fore.YELLOW, base_filename):50}', end='')
 
-        with os.scandir(Path(modlet_dir)) as modlets:
+        with scandir(Path(modlet_dir)) as modlets:
 
             for modlet in modlets:
                 filename = Path(modlet)
 
                 if modlet.is_file() and filename.suffix == '.xml':
-                    for line in etree.parse(str(filename)).getroot():
-
-                        # As of now, we can only validate the `set` commands
-                        if line.tag == 'set' and 'xpath' in line.attrib:
+                    for line in etree.parse(str(filename), parser).getroot():
+                        if 'xpath' in line.attrib:
                             xpath = line.attrib['xpath']
                             new_value = line.text
-                            attrib = xpath.split('@')[-1]
+                            tag = line.tag
+                            attrib = None
+                            if tag in ['set', 'removeattribute']:
+                                attrib = xpath.split('@')[-1]
+                            if tag == 'setattribute':
+                                attrib = line.attrib['name']
 
                             # add a / to the beginning of xpath if it's not already there
                             if xpath[0] != '/':
@@ -58,28 +68,39 @@ def readModlets(original_configs):
 
                             if options['debug']:
                                 print('\nFILE:', base_filename)
+                                print('TAG:', tag)
                                 print('XPATH:', xpath)
                                 print('RESULTS:', results)
                                 print('ATTRIB:', attrib)
                                 print('NEW VALUE:', new_value)
 
                             if results:
-                                for result in results:
-                                    result.getparent().set(attrib, new_value)
+                                if attrib:
+                                    for result in results:
+                                        if tag in ['set', 'setattribute']:
+                                            result.getparent().set(attrib, new_value)
+                                            continue
+
+                                        if tag == 'removeattribute':
+                                            result.getparent().removeattribute(attrib)
+                                            continue
+
+                                        break
                             else:
                                 passfail = False
                                 print(
-                                    f'\n{Fore.RED}FAIL: {Style.RESET_ALL}{filename} on recipe:\n{line.values()[0]}')
+                                    f'\n{colortext(Fore.RED, "FAIL: ")}{filename} on line {line.sourceline}:\n{line.values()[0]}')
 
         if passfail and options['verbose']:
-            print(f'{Fore.GREEN} OKAY {Style.RESET_ALL}')
+            print(f'{colortext(Fore.GREEN, "OKAY")}')
 
 
 def help():
     print(
         f'Usage: {Path(sys.argv[0]).name} -c <directory> [opts]\n\nOpts:')
-    print('\t-c|--config <directory>: the game config directory we should use')
-    print('\t-h|--help: this help message')
+    print('\t-c|--config <directory>: the game XML config directory')
+    print('\t-m|--modlets <directory>: where we should look for modlets')
+    print('\n\t-h|--help: this help message')
     print('\t-v|--verbose: display successes as well as failures during run -- failures always show')
     print('\n\t-d|--debug: produces copious amounts of output, not recommended for normal use!')
     sys.exit(2)
@@ -88,11 +109,12 @@ def help():
 options = {
     'config': None,
     'debug': False,
+    'modlets': ['.'],
     'verbose': False,
 }
 
-short_args = 'c:dhv'
-long_args = ['config', 'debug', 'help', 'verbose']
+short_args = 'c:dhm:v'
+long_args = ['config', 'debug', 'help', 'modlets', 'verbose']
 
 try:
     arguments, values = getopt.getopt(sys.argv[1:], short_args, long_args)
@@ -118,7 +140,7 @@ if options['config'] == None:
     help()
 
 if not Path(options['config']).exists():
-    print(f'"{options["config"]}" is not a valid directory')
+    print(f'{options["config"]!r} is not a valid directory')
     sys.exit(1)
 
 configXML = readConfigs(options['config'])
