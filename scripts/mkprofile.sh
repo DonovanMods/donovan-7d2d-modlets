91d3236d3e8d76c1e6b6be2cb574e554dc8bd818
Donovan Young <dyoung522@gmail.com>
2023-04-20 22:30:38 -0400

    Quick fix for SimpleUI

--- scripts/mkprofile.sh
+++ scripts/mkprofile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BASE="https://github.com/donovan522/donovan-7d2d-modlets/tree/stable"
+BASE="https://github.com/DonovanMods/donovan-7d2d-modlets/tree/stable"
 
 for D in $(ls -d donovan-*); do
   echo "${BASE}/${D}"
