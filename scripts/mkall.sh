ee384b3b564d12a9417ab7204302c606aa0f1030
Donovan Young <dyoung522@gmail.com>
2023-10-07 00:57:08 -0400

    Tweak current dukes location in bigbackpack

--- scripts/mkall.sh
+++ scripts/mkall.sh
@@ -5,4 +5,5 @@ ruby scripts/mk_lessgrind.rb
 ruby scripts/mk_mod_schematic_recipes.rb >modlets/donovan-modschematics/config/recipes.xml
 ruby scripts/mk_parts_recipes.rb >modlets/donovan-craftableparts/config/recipes.xml
 scripts/mkaio.sh
+ruby scripts/ck_version.rb
 scripts/mkzips.sh
